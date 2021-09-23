466d0284affcd3d0f47985b8a70e58bec7b785e9
likuan.zhou <19921105zhou>
2021-09-23 08:31:13 +0800

    feat: redis缓存工具类md文件提交

--- common-redis-test/src/main/java/com/zlk/redis/RedisApplication.java
+++ common-redis-test/src/main/java/com/zlk/redis/RedisApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-//扫描到配置的redis工具类与其他需要spring管理的bean
+//扫描到配置的redis工具类与配置类及其他需要spring管理的bean
 @ComponentScan(basePackages={"com.zlk"})
+//需要排除阿里默认数据源，否则启动报错
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class RedisApplication {
 
